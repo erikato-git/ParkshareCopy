6fa18866e187d66c49e8a3d7c5b141e79f4b76b2
Erikato <erik.kato@hotmail.com>
2022-12-29 15:55:31 +0100

    DataContext (Persistence) + Account(Domain)

---
+++ Domain/Account.cs
@@ -0,0 +1,12 @@
+namespace Domain
+{
+    public class Account
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+        public string Password { get; set; }
+        //TODO: Parkingspot
+    }
+}
